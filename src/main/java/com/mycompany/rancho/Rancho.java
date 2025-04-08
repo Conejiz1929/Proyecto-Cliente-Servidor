1920c7fd4f00586a0029223a0df295bc427d261b
Conej <Conej@Jose>
2025-04-07 20:12:05 -0600

    Interfaz grafica

---
+++ src/main/java/com/mycompany/rancho/Rancho.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rancho;
+import interfaz.MenuPrincipal;
+
+/**
+ *
+ * @author Conej
+ */
+public class Rancho {
+
+    public static void main(String[] args) {
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.setVisible(true);
+    }
+}
